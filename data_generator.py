15ebcab0bb1daee2853f59992c80adab5a18c398
Roman Inflianskas <infroma@gmail.com>
2020-09-13 10:32:05 +0300

    Cleanup

--- data_generator.py
+++ data_generator.py
@@ -27,7 +27,7 @@ def nodes(
 
     if not all_nodes_file_path.is_file():
         with open(str(all_nodes_file_path), "w") as f:
-            print("Writing concepts from database into CSV:")
+            print("Writing items from database into CSV:")
             f.write("uri\n")
             with arangodb.instance(
                     connection_uri=connection_uri,
@@ -52,56 +52,16 @@ def nodes(
 
     random_nodes_file_path = output_dir / "random_nodes.csv"
     if not random_nodes_file_path.is_file():
-        print("Read concepts from CSV into dask DataFrame")
+        print("Read items from CSV into dask DataFrame")
         df = dd.read_csv(all_nodes_file_path, keep_default_na=False)
         if count is None:
             frac = 1.0
         else:
             frac = count / len(df.index)
-        print("Writing random concepts from dask DataFrame into CSV")
+        print("Writing random items from dask DataFrame into CSV")
         df.sample(frac=frac).to_csv(str(random_nodes_file_path), index=None, single_file=True)
     else:
         print(f"File exists, skipping creation: {random_nodes_file_path}")
-
-
-@app.command()
-def all_languages(database_path_hint: Path, output_dir: Path):
-    from lightning_conceptnet import LightningConceptNet
-
-    database_path_hint = database_path_hint.expanduser()
-    lcn = LightningConceptNet(database_path_hint)
-    languages = set()
-    total = lcn._db.node_table.records()
-    print("Reading concepts from database to find all languages:")
-    with lcn.read_transaction as txn:
-        for i, concept in tqdm(enumerate(iter(lcn.concept(txn))), unit=" concepts", unit_scale=True, total=total):
-            languages.add(concept.language)
-    output_file_path = output_dir / "languages.txt"
-    with open(str(output_file_path), "w") as f:
-        for language in languages:
-            f.write(f"{language}\n")
-
-
-@app.command()
-def languages(output_dir: Path):
-    from conceptnet5.db.query import AssertionFinder
-
-    af = AssertionFinder()
-
-    input_file_path = output_dir / "languages.txt"
-    output_file_path = output_dir / "small_languages.csv"
-    with open(str(input_file_path)) as f_in:
-        total = len(f_in.readlines())
-    with open(str(input_file_path)) as f_in:
-        with open(str(output_file_path), "w") as f_out:
-            print("Writing small languages to CSV file:")
-            f_out.write("language\n")
-            for line in tqdm(f_in, total=total, unit=" languages"):
-                language = line.strip()
-                uri = f"/c/{language}"
-                l = len(af.lookup(uri, limit=None))
-                if l < 10_000:
-                    f_out.write(line)
 
 
 if __name__ == "__main__":
