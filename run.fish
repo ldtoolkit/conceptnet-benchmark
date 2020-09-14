382ade3bc04d1eeaa1451145a82813fadfe7343b
Roman Inflianskas <infroma@gmail.com>
2020-09-14 11:51:15 +0300

    Copy conceptnet-benchmark to ~conceptnet/

--- run.fish
+++ run.fish
@@ -45,7 +45,7 @@ end
 echo "Applying SaltStack state (this could take minutes to complete)"
 salt '*' state.apply
 
-cp -a (pwd) /home/conceptnet/
+cp -a $BASEDIR /home/conceptnet/
 
 echo "Swithing to conceptnet user and running all rules"
 sudo -u conceptnet /home/conceptnet/conceptnet_rocks_virtualenv/bin/snakemake --snakefile /home/conceptnet/conceptnet-benchmark/Snakefile -j1
