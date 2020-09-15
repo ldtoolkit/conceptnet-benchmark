916b4a003746708383abc50d1afa583d9bcb8ca9
Roman Inflianskas <infroma@gmail.com>
2020-09-15 15:17:13 +0300

    Fix run_benchmark.fish

--- run_benchmark.fish
+++ run_benchmark.fish
@@ -6,10 +6,12 @@ if not set -q argv[1]
   exit 1
 end
 
-/home/conceptnet/conceptnet_rocks_virtualenv/bin/python /home/conceptnet/conceptnet_rocks_virtualenv/bin/conceptnet-rocks start-arangodb &
-
-while not /home/conceptnet/conceptnet_rocks_virtualenv/bin/python /home/conceptnet/conceptnet_rocks_virtualenv/bin/conceptnet-rocks is-arangodb-running
-  sleep 0.1
+if [ $library = "conceptnet_rocks" ]
+  /home/conceptnet/conceptnet_rocks_virtualenv/bin/python /home/conceptnet/conceptnet_rocks_virtualenv/bin/conceptnet-rocks start-arangodb &
+  
+  while not /home/conceptnet/conceptnet_rocks_virtualenv/bin/python /home/conceptnet/conceptnet_rocks_virtualenv/bin/conceptnet-rocks is-arangodb-running
+    sleep 0.1
+  end
 end
 
 for what in node relation source dataset edge_uri
@@ -17,4 +19,6 @@ for what in node relation source dataset edge_uri
   "/home/conceptnet/"$library"_virtualenv/bin/python" /home/conceptnet/conceptnet-benchmark/system_requirements.py "/home/conceptnet/"$library"_virtualenv/bin/python" /home/conceptnet/conceptnet-benchmark/benchmark.py /home/conceptnet/benchmark_data/random_$what.csv $library >"/home/conceptnet/results/"$library"_"$what"_time_profile.txt" ^"/home/conceptnet/results/"$library"_"$what"_profile.txt"
 end
 
-/home/conceptnet/conceptnet_rocks_virtualenv/bin/python /home/conceptnet/conceptnet_rocks_virtualenv/bin/conceptnet-rocks stop-arangodb
+if [ $library = "conceptnet_rocks" ]
+  /home/conceptnet/conceptnet_rocks_virtualenv/bin/python /home/conceptnet/conceptnet_rocks_virtualenv/bin/conceptnet-rocks stop-arangodb
+end
